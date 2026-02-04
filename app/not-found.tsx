a56a17f16799fbb8214afbed2b6f67fe0e51e177
Developer-DaveRam <daveram@skyraan.com>
2026-02-04 13:17:53 +0530

    Update not-found details for reviews

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,15 @@
-export default function NotFound(){
-    return <h1>Page Not Found</h1>
+"use client";
+
+import { usePathname } from "next/navigation";
+
+export default function NotFound() {
+  const pathname = usePathname();
+  console.log("pathname", pathname);
+  const productId = pathname.split("/")[2];
+  const reviewId = pathname.split("/")[4];
+  return (
+    <h1>
+      Page Not Found For Review {reviewId} And ProductId {productId}
+    </h1>
+  );
 }
